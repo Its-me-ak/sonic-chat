00c8e59a6c238b40a24e43a3de6b0c77abeb58c5
Mohd Aquib <akibkhan9984@gmail.com>
2025-04-29 13:44:11 +0530

    Add Kinde authentication integration: update dependencies and implement AuthButton component

--- src/app/auth/AuthButton.tsx
+++ src/app/auth/AuthButton.tsx
@@ -1,15 +1,28 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import React, { useState } from "react";
 
 const AuthButton = () => {
-  return (
-    <div className='flex gap-3 flex-1 md:flex-row flex-col relative z-50'>
-        <Button className='w-1/2' variant={'outline'}>
-            Sign Up
-        </Button>
-        <Button className='w-1/2'>Login</Button>
-    </div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(false);
 
-export default AuthButton
+  return (
+    <div className="flex gap-3 flex-1 md:flex-row flex-col relative z-50">
+      <RegisterLink className="flex-1" onClick={() => setIsLoading(true)}>
+        <Button className="w-full" variant={"outline"} disabled={isLoading}>
+          Sign Up
+        </Button>
+      </RegisterLink>
+      <LoginLink className="flex-1" onClick={() => setIsLoading(true)}>
+        <Button className="w-full" disabled={isLoading}>
+          Login
+        </Button>
+      </LoginLink>
+    </div>
+  );
+};
+
+export default AuthButton;
