d1237a8ddf02030ba2e66a5729536ff8b0158576
Mohd Aquib <akibkhan9984@gmail.com>
2025-05-01 14:51:40 +0530

    Add message retrieval functionality and loading skeleton: implement getMessages action for fetching messages from Redis, update MessageBody to utilize getMessages, and create MessageSkeleton for loading states.

--- src/components/chat/MessageBody.tsx
+++ src/components/chat/MessageBody.tsx
@@ -1,72 +1,105 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { messages, USERS } from "@/db/dummy";
 import { cn } from "@/lib/utils";
 import { Avatar, AvatarImage } from "../ui/avatar";
+import { useSelectedUsers } from "@/store/useSelectedUsers";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useQuery } from "@tanstack/react-query";
+import { getMessages } from "@/action/message.actions";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 
 const MessageBody = () => {
-  const selectedUser = USERS[0];
-  const currentUser = USERS[1];
+  const messagesRef = useRef<HTMLDivElement>(null)
+  const { selectedUser } = useSelectedUsers();
+  const { user: currentUser, isLoading: isUserLoading } =
+    useKindeBrowserClient();
+
+  const { data: messages, isLoading: isMessagesLoading } = useQuery({
+    queryKey: ["messages", selectedUser?.id],
+    queryFn: async () => {
+      if (selectedUser && currentUser) {
+        return await getMessages(selectedUser?.id, currentUser?.id);
+      }
+    },
+    enabled: !!selectedUser && !!currentUser && !isUserLoading,
+  });
+
+  useEffect(() => {
+    if (messagesRef.current) {
+      messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+    }
+  }, [messages])
+
   return (
-    <div className="flex flex-col w-full h-full overflow-x-hidden overflow-y-auto">
+    <div ref={messagesRef} className="flex flex-col w-full h-full overflow-x-hidden overflow-y-auto">
       <AnimatePresence>
-        {messages.map((message) => (
-          <motion.div
-            key={message.id}
-            layout
-            initial={{ opacity: 0, scale: 1, y: 50, x: 0 }}
-            animate={{ opacity: 1, scale: 1, y: 0, x: 0 }}
-            exit={{ opacity: 0, scale: 1, y: 1, x: 0 }}
-            transition={{
-              opacity: { duration: 0.1 },
-              layout: {
-                type: "spring",
-                bounce: 0.3,
-                duration: messages.indexOf(message) * 0.05 + 0.2,
-              },
-            }}
-            style={{
-              originX: 0.5,
-              originY: 0.5,
-            }}
-            className={cn(
-              "flex flex-col gap-2 p-4 whitespace-pre-wrap",
-              message.senderId === currentUser?.id ? "items-end" : "items-start"
-            )}
-          >
-            <div className="flex gap-2.5 items-center">
-              {message.senderId === selectedUser.id && (
-                <Avatar className="flex justify-center items-center">
-                  <AvatarImage
-                    src={selectedUser.image}
-                    alt={selectedUser.name}
-                    className="border-2 border-white rounded-full"
+        {!isMessagesLoading &&
+          messages?.map((message) => (
+            <motion.div
+              key={message.id}
+              layout
+              initial={{ opacity: 0, scale: 1, y: 50, x: 0 }}
+              animate={{ opacity: 1, scale: 1, y: 0, x: 0 }}
+              exit={{ opacity: 0, scale: 1, y: 1, x: 0 }}
+              transition={{
+                opacity: { duration: 0.1 },
+                layout: {
+                  type: "spring",
+                  bounce: 0.3,
+                  duration: messages.indexOf(message) * 0.05 + 0.2,
+                },
+              }}
+              style={{
+                originX: 0.5,
+                originY: 0.5,
+              }}
+              className={cn(
+                "flex flex-col gap-2 p-4 whitespace-pre-wrap",
+                message.senderId === currentUser?.id
+                  ? "items-end"
+                  : "items-start"
+              )}
+            >
+              <div className="flex gap-2.5 items-center">
+                {message.senderId === selectedUser?.id && (
+                  <Avatar className="flex justify-center items-center">
+                    <AvatarImage
+                      src={selectedUser.image}
+                      alt={selectedUser.name}
+                      className="border-2 border-white rounded-full"
+                    />
+                  </Avatar>
+                )}
+                {message.messageType === "text" ? (
+                  <span className="bg-accent p-2 rounded-md max-w-xs">
+                    {message.content}
+                  </span>
+                ) : (
+                  <img
+                    src={message.content}
+                    alt="Message Image"
+                    className="border p-2 rounded h-40 md:h-52 object-cover"
                   />
-                </Avatar>
-              )}
-              {message.messageType === "text" ? (
-                <span className="bg-accent p-2 rounded-md max-w-xs">
-                  {message.content}
-                </span>
-              ) : (
-                <img
-                  src={message.content}
-                  alt="Message Image"
-                  className="border p-2 rounded h-40 md:h-52 object-cover"
-                />
-              )}
-              {message.senderId === currentUser.id && (
-                <Avatar className="flex justify-center items-center">
-                  <AvatarImage
-                    src={currentUser.image}
-                    alt={currentUser.name}
-                    className="border-2 border-white rounded-full"
-                  />
-                </Avatar>
-              )}
-            </div>
-          </motion.div>
-        ))}
+                )}
+                {message.senderId === currentUser?.id && (
+                  <Avatar className="flex justify-center items-center">
+                    <AvatarImage
+                      src={currentUser?.picture || "/user-placeholder.png"}
+                      alt={currentUser?.picture || "User Image"}
+                      className="border-2 border-white rounded-full"
+                    />
+                  </Avatar>
+                )}
+              </div>
+            </motion.div>
+          ))}
+        {isMessagesLoading && (
+          <>
+            <MessageSkeleton />
+            <MessageSkeleton />
+            <MessageSkeleton />
+          </>
+        )}
       </AnimatePresence>
     </div>
   );
